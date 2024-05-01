54ec92268442958093f08555c35fa987d89a46f4
spsalmon <psalmonsacha@gmail.com>
2024-05-01 15:29:02 +0200

    switching to Qt

--- src/napari_worm_nuclei/_widget.py
+++ src/napari_worm_nuclei/_widget.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING
 
 from magicgui.widgets import Container, create_widget
-from qtpy.QtWidgets import QHBoxLayout
+from qtpy.QtWidgets import QVBoxLayout, QFileDialog, QWidget
 import os
 
 if TYPE_CHECKING:
@@ -10,23 +10,44 @@ if TYPE_CHECKING:
 
 # if we want even more control over our widget, we can use
 # magicgui `Container`
-class DataReader(Container):
+# class DataReader(Container):
+#     def __init__(self, viewer: "napari.viewer.Viewer"):
+#         super().__init__()
+#         self._viewer = viewer
+
+#         # create a widget allowing the user to select a folder containing images
+#         self.img_dir = create_widget(annotation=os.PathLike, label="Image directory")
+#         self.img_dir.changed.connect(self._on_img_dir_change)
+
+#         self.mask_dir = create_widget(annotation=os.PathLike, label="Mask directory")
+#         self.mask_dir.changed.connect(self._on_mask_dir_change)
+
+#     def _on_img_dir_change(self, event):
+#         print(self.img_dir.value)
+
+#     def _on_mask_dir_change(self, event):
+#         print(self.mask_dir.value)
+
+class DataReader(QWidget):
     def __init__(self, viewer: "napari.viewer.Viewer"):
         super().__init__()
-        self._viewer = viewer
+        self.viewer = viewer
 
-        # create a widget allowing the user to select a folder containing images
-        self.img_dir = create_widget(annotation=os.PathLike, label="Image directory")
-        self.img_dir.changed.connect(self._on_img_dir_change)
+        self.img_dir = QFileDialog()
+        self.mask_dir = QFileDialog()
 
-        self.mask_dir = create_widget(annotation=os.PathLike, label="Mask directory")
-        self.mask_dir.changed.connect(self._on_mask_dir_change)
+        self.img_dir.textChanged.connect(self._on_img_dir_change)
+        self.mask_dir.textChanged.connect(self._on_mask_dir_change)
+
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.img_dir)
+        self.layout().addWidget(self.mask_dir)
 
     def _on_img_dir_change(self, event):
-        print(self.img_dir.value)
+        print(self.img_dir.text())
 
     def _on_mask_dir_change(self, event):
-        print(self.mask_dir.value)
+        print(self.mask_dir.text())
 
 # class ExampleQWidget(QWidget):
 #     # your QWidget.__init__ can optionally request the napari viewer instance
