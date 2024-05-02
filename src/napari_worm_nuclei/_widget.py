f2cd6d79a3159089bd11ee8ded295f423b9cf170
spsalmon <psalmonsacha@gmail.com>
2024-05-02 08:31:39 +0200

    trying out refactored code

--- src/napari_worm_nuclei/_widget.py
+++ src/napari_worm_nuclei/_widget.py
@@ -49,134 +49,109 @@ class DataReader(QWidget):
     def __init__(self, viewer: "napari.viewer.Viewer"):
         super().__init__()
         self.viewer = viewer
+        self.setup_ui()
 
-        # Set up the layout
-        layout = QVBoxLayout()
+    def setup_ui(self):
+        self.layout = QVBoxLayout()
+        self.setup_directory_selection()
+        self.setup_file_controls()
+        self.setup_navigation_controls()
+        self.setup_load_button()
+        self.setLayout(self.layout)
 
-        self.img_dir_path = ""
-        self.mask_dir_path = ""
-        self.filemap = pd.DataFrame()
+    def setup_directory_selection(self):
+        self.img_dir_path, self.mask_dir_path = "", ""
+        self.img_dir_edit, self.img_dir_button = self.create_dir_selector("Select Image Directory")
+        self.mask_dir_edit, self.mask_dir_button = self.create_dir_selector("Select Mask Directory")
 
-        # Create widgets for image directory selection
-        self.img_dir_edit = QLineEdit()  # To display the path
-        self.img_dir_button = QPushButton("Select Image Directory")
-        self.img_dir_button.clicked.connect(self.select_img_dir)
-        layout.addWidget(self.img_dir_edit)
-        layout.addWidget(self.img_dir_button)
+    def create_dir_selector(self, button_label):
+        dir_edit = QLineEdit()
+        dir_button = QPushButton(button_label)
+        dir_button.clicked.connect(lambda: self.select_directory(dir_edit))
+        self.layout.addWidget(dir_edit)
+        self.layout.addWidget(dir_button)
+        return dir_edit, dir_button
 
-        # Create widgets for mask directory selection
-        self.mask_dir_edit = QLineEdit()  # To display the path
-        self.mask_dir_button = QPushButton("Select Mask Directory")
-        self.mask_dir_button.clicked.connect(self.select_mask_dir)
-        layout.addWidget(self.mask_dir_edit)
-        layout.addWidget(self.mask_dir_button)
+    def select_directory(self, dir_edit):
+        dir_path = QFileDialog.getExistingDirectory(self, "Select Directory")
+        if dir_path:
+            dir_edit.setText(dir_path)
+            return dir_path
+        else:
+            print("Directory selection cancelled.")
+            return ""
 
-        # Add a button to get the files from the selected directories
+    def setup_file_controls(self):
         self.get_files_button = QPushButton("Get Files")
-        layout.addWidget(self.get_files_button)
         self.get_files_button.clicked.connect(self.get_files)
+        self.layout.addWidget(self.get_files_button)
 
-        # Add a time and point selection widget
-        self.previous_time_button = QPushButton("Previous Time")
-        self.time_combo = QComboBox()
-        self.next_time_button = QPushButton("Next Time")
+    def setup_navigation_controls(self):
+        self.setup_time_navigation()
+        self.setup_point_navigation()
 
-        self.previous_time_button.clicked.connect(self.previous_time)
-        self.next_time_button.clicked.connect(self.next_time)
+    def setup_time_navigation(self):
+        self.previous_time_button, self.time_combo, self.next_time_button = self.create_navigation_controls("Time")
+        self.layout.addLayout(self.create_horizontal_layout(self.previous_time_button, self.time_combo, self.next_time_button))
 
-        self.previous_point_button = QPushButton("Previous Point")
-        self.point_combo = QComboBox()
-        self.next_point_button = QPushButton("Next Point")
+    def setup_point_navigation(self):
+        self.previous_point_button, self.point_combo, self.next_point_button = self.create_navigation_controls("Point")
+        self.layout.addLayout(self.create_horizontal_layout(self.previous_point_button, self.point_combo, self.next_point_button))
 
-        self.previous_point_button.clicked.connect(self.previous_point)
-        self.next_point_button.clicked.connect(self.next_point)
+    def create_navigation_controls(self, label):
+        prev_button = QPushButton(f"Previous {label}")
+        next_button = QPushButton(f"Next {label}")
+        combo = QComboBox()
+        prev_button.clicked.connect(lambda: self.navigate(combo, -1))
+        next_button.clicked.connect(lambda: self.navigate(combo, 1))
+        return prev_button, combo, next_button
 
-        # Put the time widgets in a horizontal layout
-        time_layout = QHBoxLayout()
-        time_layout.addWidget(self.previous_time_button)
-        time_layout.addWidget(self.time_combo)
-        time_layout.addWidget(self.next_time_button)
+    def create_horizontal_layout(self, *widgets):
+        layout = QHBoxLayout()
+        for widget in widgets:
+            layout.addWidget(widget)
+        return layout
 
-        # Put the point widgets in a horizontal layout
-        point_layout = QHBoxLayout()
-        point_layout.addWidget(self.previous_point_button)
-        point_layout.addWidget(self.point_combo)
-        point_layout.addWidget(self.next_point_button)
+    def navigate(self, combo, direction):
+        new_index = combo.currentIndex() + direction
+        if 0 <= new_index < combo.count():
+            combo.setCurrentIndex(new_index)
 
-        # Add the time and point layouts to the main layout
-        layout.addLayout(time_layout)
-        layout.addLayout(point_layout)
-
-        # Add a button to load the selected image and mask
+    def setup_load_button(self):
         self.load_button = QPushButton("Load Images")
-        layout.addWidget(self.load_button)
         self.load_button.clicked.connect(self.load_images)
-
-        # Set the layout to the QWidget
-        self.setLayout(layout)
-
-    def select_img_dir(self):
-        dir_path = QFileDialog.getExistingDirectory(self, "Select Image Directory")
-        if dir_path:
-            self.img_dir_edit.setText(dir_path)
-            self.img_dir_path = dir_path
-
-    def select_mask_dir(self):
-        dir_path = QFileDialog.getExistingDirectory(self, "Select Mask Directory")
-        if dir_path:
-            self.mask_dir_edit.setText(dir_path)
-            self.mask_dir_path = dir_path
+        self.layout.addWidget(self.load_button)
 
     def get_files(self):
-        print("Getting files from the selected directories")
-        print("Image directory:", self.img_dir_path)
-        print("Mask directory:", self.mask_dir_path)
+        if self.img_dir_edit.text() and self.mask_dir_edit.text():
+            self.load_file_map(self.img_dir_edit.text(), self.mask_dir_edit.text())
+            self.populate_combos()
+        else:
+            print("Please select both image and mask directories.")
 
-        filemap = file_handling.get_dir_filemap(self.img_dir_path)
-        filemap = add_dir_to_experiment_filemap(filemap, self.mask_dir_path, "MaskPath")
-
-        # Remove all rows with NaN values
-        filemap = filemap.dropna()
-
-        # Remove all rows with empty strings
-        filemap = filemap[(filemap != "").all(axis=1)]
-
-        self.filemap = filemap
-
-        # Reset the combo boxes
-        self.time_combo.clear()
-        self.point_combo.clear()
-
-        # Populate the time and point combo boxes
-        time_values = [str(time) for time in filemap["Time"].unique()]
-        self.time_combo.addItems(time_values)
-
-        point_values = [str(point) for point in filemap["Point"].unique()]
-        self.point_combo.addItems(point_values)
-
-    def previous_time(self):
-        # select the previous element in the time combo box
-        current_index = self.time_combo.currentIndex()
-        if current_index > 0:
-            self.time_combo.setCurrentIndex(current_index - 1)
+    # def previous_time(self):
+    #     # select the previous element in the time combo box
+    #     current_index = self.time_combo.currentIndex()
+    #     if current_index > 0:
+    #         self.time_combo.setCurrentIndex(current_index - 1)
     
-    def next_time(self):
-        # select the next element in the time combo box
-        current_index = self.time_combo.currentIndex()
-        if current_index < self.time_combo.count() - 1:
-            self.time_combo.setCurrentIndex(current_index + 1)
+    # def next_time(self):
+    #     # select the next element in the time combo box
+    #     current_index = self.time_combo.currentIndex()
+    #     if current_index < self.time_combo.count() - 1:
+    #         self.time_combo.setCurrentIndex(current_index + 1)
     
-    def previous_point(self):
-        # select the previous element in the point combo box
-        current_index = self.point_combo.currentIndex()
-        if current_index > 0:
-            self.point_combo.setCurrentIndex(current_index - 1)
+    # def previous_point(self):
+    #     # select the previous element in the point combo box
+    #     current_index = self.point_combo.currentIndex()
+    #     if current_index > 0:
+    #         self.point_combo.setCurrentIndex(current_index - 1)
     
-    def next_point(self):
-        # select the next element in the point combo box
-        current_index = self.point_combo.currentIndex()
-        if current_index < self.point_combo.count() - 1:
-            self.point_combo.setCurrentIndex(current_index + 1)
+    # def next_point(self):
+    #     # select the next element in the point combo box
+    #     current_index = self.point_combo.currentIndex()
+    #     if current_index < self.point_combo.count() - 1:
+    #         self.point_combo.setCurrentIndex(current_index + 1)
 
     def load_images(self):
         time = int(self.time_combo.currentText())
@@ -190,17 +165,13 @@ class DataReader(QWidget):
         # Read images using tifffile
         img_data = tifffile.imread(img_path)
         mask_data = tifffile.imread(mask_path)
+        # add a dimension to the mask data if img_data has 4 dimensions
+        if img_data.ndim == 4:
+            mask_data = np.expand_dims(mask_data, axis=0)
 
         # Add images to Napari viewer
         self.viewer.add_image(img_data, name=f"Image at time {time} point {point}")
         self.viewer.add_labels(mask_data, name=f"Mask at time {time} point {point}")
-
-
-
-
-
-
-
 
 
 # class ExampleQWidget(QWidget):
