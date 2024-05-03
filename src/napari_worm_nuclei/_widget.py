738e7117771a59423de326bfe8a85dbfc3c47055
spsalmon <psalmonsacha@gmail.com>
2024-05-03 16:39:42 +0200

    maybe fixed the colors

--- src/napari_worm_nuclei/_widget.py
+++ src/napari_worm_nuclei/_widget.py
@@ -408,8 +408,8 @@ class AnnotationTool(QWidget):
                     self.points_layer.data = np.append(self.points_layer.data, np.array([point]), axis=0)
 
                     # Add the predicted class to the annotation layer
-                    print(f'Color {self.class_values_to_color[prediction]}')
-                    self.points_layer.face_color = np.append(self.points_layer.face_color, np.array([self.class_values_to_color[prediction]]))
+                    print(f'Color {np.array([self.class_values_to_color[prediction]]).astype(float)}')
+                    self.points_layer.face_color = np.append(self.points_layer.face_color, np.array([self.class_values_to_color[prediction]]).astype(float), axis=0)
 
 # class ExampleQWidget(QWidget):
 #     # your QWidget.__init__ can optionally request the napari viewer instance
