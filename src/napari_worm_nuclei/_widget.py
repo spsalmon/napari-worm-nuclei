7616882f1f654caff82fac952d48355022b4f64f
spsalmon <psalmonsacha@gmail.com>
2024-05-06 12:34:16 +0200

    debugging point add

--- src/napari_worm_nuclei/_widget.py
+++ src/napari_worm_nuclei/_widget.py
@@ -340,7 +340,12 @@ class AnnotationTool(QWidget):
             centroid = np.mean(np.argwhere(label_mask), axis=0)
             # Add the centroid to the annotation layer
             point = np.array([centroid[0], centroid[1]])
-            self.points_layer.data = np.append(self.points_layer.data, point, axis=0)
+            print(point)
+            print(f'point layer data shape: {self.points_layer.data.shape}')
+            if self.points_layer.data.shape[0] == 0:
+                self.points_layer.data = np.array([point])
+            else:
+                self.points_layer.data = np.append(self.points_layer.data, point, axis=0)
 
             # Add the predicted class to the annotation layer
             self.points_layer.face_color[-1] = np.array(self.class_values_to_color[prediction]).astype(float)
